1a438d992afa2ff9f89b7d8f71dc0594fa34dc06
magnet (skid) <86327263+carolesdaughter@users.noreply.github.com>
2023-04-13 16:55:18 -0700

    Update Hai.lua

--- Hai.lua
+++ Hai.lua
@@ -6,13 +6,13 @@ function Utility:GetRandomSubtitle()
     return Subtitles[math.random(1, #Subtitles)]
 end
 
-function Utility:GetSubPrefix(String)
+function Utility:GetSuffix(String)
     String = tostring(String):gsub(" ", "")
     
     local Prefix = ""
 
     if #String == 2 then
-        local Magnettttttt = string.sub(String, #String, #String+1)
+        local Magnettttttt = string.sub(String, #String, #String + 1)
         
         Prefix = Oh == "1" and "st" or sec == "2" and "nd" or sec == "3" and "rd" or "th"
     end
